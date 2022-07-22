ece0d9a3c1fbbf23aa87f6c1f493edde8d9d6f0d
chldudgus <108377410+chldudgus@users.noreply.github.com>
2022-07-22 17:42:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-    $("#bthome").bind('click',function () {
-      $("html, body").animate({scrollTop:'0'},500);
+  $('#btmain').bind('click', function() {
+    $("html, body").animate({scrollTop: '0'}, 500);
   });
-  
-  $('#bthome').on('click', function() {
+
+  $('#btmain').on('click', function () {
     $('html').scrollTop('0');
   });
+});
